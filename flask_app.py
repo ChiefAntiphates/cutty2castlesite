9c8d75ac271e5fb140fcc37fd4f337240032ee7e
Harry <hw00441@surrey.ac.uk>
2021-05-03 21:49:50 +0100

    Added requirements.txt

--- flask_app.py
+++ flask_app.py
@@ -20,3 +20,12 @@ def goodbye():
     
 if __name__ == "__main__":
     app.run(debug=True)
+
+#API request to get donations in JSON format of a thing
+#curl --request GET -H "Content-type: application/json" https://api.justgiving.com/137f78b6/v1/fundraising/pages/mark-clark6/donations
+#curl --request GET -H "Content-type: application/json" https://api.justgiving.com/137f78b6/v1/fundraising/pages/mark-clark6/
+
+#Use web scraping to get the grand total & number of donations from against malaria
+
+
+#Receive emails as Python, update a database somewhere?
